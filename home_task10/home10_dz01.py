be6b15f1b0f1eb49b1d8ccaca8e0f188766903bd
Oleksii <Savchenko>
2018-07-18 11:52:47 +0300

    home_work

---
+++ home_task10/home10_dz01.py
@@ -0,0 +1,9 @@
+"""
+задача 1
+Переписать информацию с одного файла в другой
+"""
+
+with open(r'output.txt', 'w') as output, open('input.txt', 'r') as input:
+    output.write(input.read())
+
+output.close()
